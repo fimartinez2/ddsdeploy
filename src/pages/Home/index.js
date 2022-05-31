7e19bdb86e23c51ef82be76cb9214d0ec43b5d55
FELIPE MARTINEZ <fimartinez2@uc.cl>
2022-05-30 20:47:22 -0400

    hotfix

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -20,14 +20,16 @@ const Home = () => {
   const goCupons = () => {
     navigate("/cupons");
   };
+
   return (
     <div className="home-div">
-      <h1>Hola {userName}</h1>
-      <ul>
-        <li>Token {userToken}</li>
-        <li>TokenType {userTokenType}</li>
-      </ul>
-      <h1>Home</h1>
+      {userName ? (
+        <>
+          <h1>Hola {userName}</h1>
+        </>
+      ) : (
+        <h1>Home</h1>
+      )}
       <h3 onClick={goProducts}>Ir a sección productos</h3>
       <h3 onClick={goTags}>Ir a sección categorías</h3>
       <h3 onClick={goOrderHistory}>Ir a sección de historial de compras</h3>
