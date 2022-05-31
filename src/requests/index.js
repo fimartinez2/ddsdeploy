21185cdd0a2b2354fb7ee60a2ff1877cf9446271
Felipe Martinez Nakousi <42326240+fimartinez2@users.noreply.github.com>
2022-05-30 20:22:26 -0400

    Update index.js

--- src/requests/index.js
+++ src/requests/index.js
@@ -2,4 +2,4 @@
 // export const headUrl = "http://127.0.0.1:3000";
 
 // PRODUCTION purposes.
-export const headUrl = "https://api.todo-app-react.herokuapp.com";
+export const headUrl = "https://ec2-3-145-76-99.us-east-2.compute.amazonaws.com";
